69725b2128c360691a815d5aa11a44cfdc05a622
robinlan <robin_lan2003@yahoo.com.tw>
2014-08-16 14:57:29 +0800

    Scientific Computing in Python

---
+++ Invent with Python V2/jokes.py
@@ -0,0 +1,16 @@
+print 'What do you get when you cross a snowman with a vampire?'
+raw_input()
+print 'Frostbite!'
+print
+print 'What do dentists call a astronaut\'s cavity?'
+raw_input()
+print 'A black hole!'
+print
+print 'Knock knock.'
+raw_input()
+print "Who's there?"
+raw_input()
+print 'Interrupting cow.'
+raw_input()
+print 'Interrupting cow wh',
+print '-MOO!'
